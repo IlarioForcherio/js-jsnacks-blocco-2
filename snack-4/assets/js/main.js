7d03d38bfb4c195f7e1b00fd111e27600495e87b
ilarioforcherio <ilarioforcherio@gmail.com>
2022-10-25 16:41:15 +0200

    end "Snack-4"

---
+++ snack-4/assets/js/main.js
@@ -0,0 +1,14 @@
+//*Snack4 (Bonus)*
+//Crea due array che hanno un numero di elementi diversi.  Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro.
+
+
+
+let arrayFirst = [1,2,3,4,5,];
+
+let arraySecond = [];
+
+
+let arrayNewNumbers = arraySecond.push(5,4,3,2,1);
+console.log(arraySecond);
+
+
