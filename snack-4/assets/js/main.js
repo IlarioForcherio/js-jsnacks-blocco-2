78f17a16c61b21390e68d62a2bf0adc7b00daf69
ilarioforcherio <ilarioforcherio@gmail.com>
2022-10-25 23:51:10 +0200

    add "Snack-4" function

--- snack-4/assets/js/main.js
+++ snack-4/assets/js/main.js
@@ -7,8 +7,23 @@ let arrayFirst = [1,2,3,4,5,];
 
 let arraySecond = [];
 
+let number = 0;
+ number = Math.floor(Math.random() * 10);
 
-let arrayNewNumbers = arraySecond.push(5,4,3,2,1);
+
+let differenzaArray = arrayFirst.length - arraySecond.length;
+
+for ( i = 0; i < differenzaArray ; i++ ){
+    
+    arraySecond[i] = arraySecond.push(number);
+
+
+}
+
 console.log(arraySecond);
+
+
+//let arrayNewNumbers = arraySecond.push(5,4,3,2,1);
+//console.log(arraySecond);
 
 
